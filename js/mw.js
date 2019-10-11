5886359e4b4a8ea680d01c1534b415bd4c11d477
farzad <farzadezk@yahoo.co.uk>
2019-10-11 02:59:12 -0700

    20191011-1

--- js/mw.js
+++ js/mw.js
@@ -1,5 +1,5 @@
 const app = angular.module("mwApp", ["ngRoute"]);
-app.config(function ($routeProvider) {
+app.config(function($routeProvider) {
     $routeProvider
         .when("/", {
             templateUrl: "pages/home.html"
@@ -9,11 +9,44 @@ app.config(function ($routeProvider) {
         });
 });
 
-app.controller('mwCtrl', function ($scope) {
-    $scope.text = texts;
+app.controller('mwCtrl', function($scope, $location, $anchorScroll) {
     $scope.MenuIsOpen = false;
+    $scope.$lang = true;
+    $scope.text = $scope.$lang ? texts.en : texts.fr;
 
-    $scope.openMenu = function () {
+    $scope.scrollTo = function(id) {
+        $location.hash(id);
+        $anchorScroll();
+    }
+    $scope.openMenu = function() {
         $scope.MenuIsOpen = !$scope.MenuIsOpen;
     }
+
+    $scope.changeLang = function() {
+        $scope.$lang = $scope.$lang ? false : true;
+        $scope.text = $scope.$lang ? texts.en : texts.fr;
+    }
+
 });
+
+
+/*
+      var token = '3648306560.1c1827a.4a2783080b7646dcac204724ad4a63fc';
+      $.ajax({
+        url: 'https://api.instagram.com/v1/users/self/media/recent',
+        dataType: 'jsonp',
+        type: 'GET',
+        data: {access_token: token},
+        success: function(data){
+          for( x in data.data ){
+            if( data.data[x].tags.indexOf("montrealweb")>-1 ){
+              $('#instafeed').append('<div><img src="'+data.data[x].images.low_resolution.url+'" alt="instagram post image"></div>');
+            }
+          }
+        },
+        error: function(data){
+          console.log(data);
+        }
+      });
+
+*/
--- js/texts.js
+++ js/texts.js
@@ -1,126 +1,142 @@
 const texts = {
-    "en":{
+    "en": {
         "menu": {
             "lang": "fr",
-            "home":"home",
-            "what_we_do":"What we do",
-            "portfolio":"portfolio",
-            "about_us":"about us",
-            "contact_us":"contact us"
+            "home": "home",
+            "what_we_do": "What we do",
+            "portfolio": "portfolio",
+            "about_us": "about us",
+            "contact_us": "contact us"
         },
-        "sec1":{
-            "slogan":"We are a small and family web studio in Longueuil. Our vision is to make beautiful websites for our clients.",
-            "tell_us_what_you_need":"Tell us what you need"
+        "sec1": {
+            "slogan": "We are a small and family web studio in Longueuil. Our vision is to make beautiful websites for our clients.",
+            "tell_us_what_you_need": "Tell us what you need"
         },
-        "sec2":{
-            "what_we_do":"What we do",
-            "web_design":"Web Design",
-            "web_development":"Web Development",
-            "responsive_Web":"Responsive Web",
-            "update_website_contents":"Update the website contents",
-            "debug_improvement":"Debug and Improvement",
-            "web_design_text":"One of our expertise is to design web. We are specialist is UI/UX design. We are Graphist and Photographer also.",
-            "web_development_front_text":"Have you ever heard about HTML, CSS, Javascript, Typescript? We are specialist in them. Angular, jQuery, Bootstrap... are some of expertises in FrontEnd.",
-            "web_development_bk_text":"We are PHP programmers, on MySQL. NodeJS and MongoDB is another part of us. This makes us able to provide most of website services.",
-            "responsive_Web_text":"Does your visitors check your website by their smart phones? Tablet? Laptop? Doesn't matter what device and how, We make the website full adaptable with any devices!",
-            "update_website_contents_text":"Do you need only to update and made some changes in your website? We can offer you a very reasonable price for a perfect service.",
-            "debug_improvement_text":"Is there any problem in your website? Do you need to make something better? Talk to us, let us find the best solution by a reasonable cost."
+        "sec2": {
+            "what_we_do": "What we do",
+            "web_design": "Web Design",
+            "web_development": "Web Development",
+            "responsive_Web": "Responsive Web",
+            "update_website_contents": "Update the website contents",
+            "debug_improvement": "Debug and Improvement",
+            "web_design_text": "One of our expertise is to design web. We are specialist is UI/UX design. We are Graphist and Photographer also.",
+            "web_development_front_text": "Have you ever heard about HTML, CSS, Javascript, Typescript? We are specialist in them. Angular, jQuery, Bootstrap... are some of expertises in FrontEnd.",
+            "web_development_bk_text": "We are PHP programmers, on MySQL. NodeJS and MongoDB is another part of us. This makes us able to provide most of website services.",
+            "responsive_Web_text": "Does your visitors check your website by their smart phones? Tablet? Laptop? Doesn't matter what device and how, We make the website full adaptable with any devices!",
+            "update_website_contents_text": "Do you need only to update and made some changes in your website? We can offer you a very reasonable price for a perfect service.",
+            "debug_improvement_text": "Is there any problem in your website? Do you need to make something better? Talk to us, let us find the best solution by a reasonable cost."
         },
-        "sec3":{
-            "some_of_our_works":"some of our works",
-            "some_of_our_works_text":"You can find here some of our works, included Web programming, web design, graphic design or photography.",
-            "portfolio":"portfolio",
-            "see_all_portfolio":"See complete portfolio",
-            "graphic_design":"Conception graphique",
-            "web_design_ui":"Conception Web (UI)",
-            "email_marketing":"publicité par e-mail"
+        "sec3": {
+            "some_of_our_works": "some of our works",
+            "some_of_our_works_text": "You can find here some of our works, included Web programming, web design, graphic design or photography.",
+            "portfolio": "portfolio",
+            "see_all_portfolio": "See complete portfolio",
+            "graphic_design": "Conception graphique",
+            "web_design_ui": "Conception Web (UI)",
+            "email_marketing": "publicité par e-mail"
         },
-        "sec4":{
-            "about_us":"about us",
-            "web_design":"Web Design",
-            "web_development":"Web Development",
-            "frontend_development":"Frontend Development",
-            "photography":"Photography",
-            "gr_design":"Graphic design",
-            "we_are_expert_in_web_text":"After years of education in IT, Computer application programming, Web technology and Infographie, and more than 10 years of experiences in big, medium or small size projects, now we can say that we are expert in some areas in web and IT."
+        "sec4": {
+            "about_us": "about us",
+            "web_design": "Web Design",
+            "web_development": "Web Development",
+            "frontend_development": "Frontend Development",
+            "photography": "Photography",
+            "gr_design": "Graphic design",
+            "we_are_expert_in_web_text": "After years of education in IT, Computer application programming, Web technology and Infographie, and more than 10 years of experiences in big, medium or small size projects, now we can say that we are expert in some areas in web and IT."
         },
         "sec5": {
-            "meet_the_team":"RENCONTRER L'ÉQUIPE",
-            "we_are_expert_in_what_we_do":"Nous sommes experts dans ce que nous faisons!",
-            "programmer_developer":"Programmeur / Développeur",
-            "farzad_text":"Développeur Web expérimenté et bien éduqué. Il a étudié en \"Programmation d'applications informatiques\", \"Technologie Web\" et \"Infographie\" et possède également plus de 6 années d'expérience dans le domaine du Web, de la programmation et de la conception.",
-            "sales_manager_accountant":"Directrice des ventes et Comptable",
-            "lili_text":"Directrice commercial et directrice financier. Elle a étudié en \"Économie théorique\" et en \"Comptabilité\". Elle a plus de 5 ans d'expérience en tant que responsable des ventes et 2 ans en tant que technicienne en comptabilité.",
-            "motivators":"Motivatrices",
-            "motivators_text":"Céline, a 4 ans et Caroline, a moin d\'un an. Ils sont experts dans leur rôle de motivateur de la vie."
+            "meet_the_team": "RENCONTRER L'ÉQUIPE",
+            "we_are_expert_in_what_we_do": "Nous sommes experts dans ce que nous faisons!",
+            "programmer_developer": "Programmeur / Développeur",
+            "farzad_text": "Développeur Web expérimenté et bien éduqué. Il a étudié en \"Programmation d'applications informatiques\", \"Technologie Web\" et \"Infographie\" et possède également plus de 6 années d'expérience dans le domaine du Web, de la programmation et de la conception.",
+            "sales_manager_accountant": "Directrice des ventes et Comptable",
+            "lili_text": "Directrice commercial et directrice financier. Elle a étudié en \"Économie théorique\" et en \"Comptabilité\". Elle a plus de 5 ans d'expérience en tant que responsable des ventes et 2 ans en tant que technicienne en comptabilité.",
+            "motivators": "Motivatrices",
+            "motivators_text": "Céline, a 4 ans et Caroline a moin d'un an. Ils sont experts dans leur rôle de motivateur de la vie."
+        },
+        "sec6": {
+            "social_network": "Social networks",
+            "insta_posts": "Posts in Instagram"
+        },
+        "sec7": {
+            "contact_us": "Contact Us",
+            "name": "Name",
+            "phone": "Phone",
+            "send_message": "Send Message",
+            "err_msg": "This box could not be empty"
         }
     },
-    "fr":{
+    "fr": {
         "menu": {
             "lang": "en",
-            "home":"accueuil",
-            "what_we_do":"Ce que nous faisons",
-            "portfolio":"portefeuille",
-            "about_us":"À propos de nous",
-            "contact_us":"contactez-nous"
+            "home": "accueuil",
+            "what_we_do": "Ce que nous faisons",
+            "portfolio": "portefeuille",
+            "about_us": "À propos de nous",
+            "contact_us": "contactez-nous"
         },
-        "sec1":{
-            "slogan":"Nous sommes un petit studio Web familial à Longueuil. Notre vision est de créer de beaux sites Web pour nos clients.",
-            "tell_us_what_you_need":"Dites-nous ce que vous avez besoin",
+        "sec1": {
+            "slogan": "Nous sommes un petit studio Web familial à Longueuil. Notre vision est de créer de beaux sites Web pour nos clients.",
+            "tell_us_what_you_need": "Dites-nous ce que vous avez besoin",
         },
-        "sec2":{
-            "what_we_do":"Ce que nous faisons",
-            "web_design":"Design Web",
-            "web_development":"Développement Web",
-            "responsive_Web":"Site Web responsive",
-            "update_website_contents":"Mettre à jour le contenu du site",
-            "debug_improvement":"Débogage et Amélioration",
-            "web_design_text":"Une de nos compétences est la conception Web. Nous sommes spécialisés dans la conception UI / UX. Nous sommes aussi graphiste et photographe.",
-            "web_development_front_text":"Avez-vous déjà entendu parler de HTML, CSS, Javascript, Typescript? Nous sommes spécialistes en eux. Angular, jQuery, Bootstrap... sont quelques-unes des expertises de FrontEnd.",
-            "web_development_bk_text":"Nous sommes des programmeurs PHP/MySQL. NodeJS/MongoDB sont autre partie de nous. Cela nous permet de fournir la plupart des services de site Web.",
-            "responsive_Web_text":"Vos visiteurs vérifient-ils votre site Web à l'aide de leurs téléphones intelligents? Tablette? Portable? Peu importe quel appareil, nous rendons le site Web totalement adaptable à tout appareil!",
-            "update_website_contents_text":"Avez-vous seulement besoin de mettre à jour et d'apporter des modifications à votre site Web? Nous pouvons vous offrir un prix très raisonnable pour un service parfait.",
-            "debug_improvement_text":"Y a-t-il un problème sur votre site web? Avez-vous besoin de faire quelque chose de mieux? Parlez-nous, laissez-nous trouver la meilleure solution à un coût raisonnable."
+        "sec2": {
+            "what_we_do": "Ce que nous faisons",
+            "web_design": "Design Web",
+            "web_development": "Développement Web",
+            "responsive_Web": "Site Web responsive",
+            "update_website_contents": "Mettre à jour le contenu du site",
+            "debug_improvement": "Débogage et Amélioration",
+            "web_design_text": "Une de nos compétences est la conception Web. Nous sommes spécialisés dans la conception UI / UX. Nous sommes aussi graphiste et photographe.",
+            "web_development_front_text": "Avez-vous déjà entendu parler de HTML, CSS, Javascript, Typescript? Nous sommes spécialistes en eux. Angular, jQuery, Bootstrap... sont quelques-unes des expertises de FrontEnd.",
+            "web_development_bk_text": "Nous sommes des programmeurs PHP/MySQL. NodeJS/MongoDB sont autre partie de nous. Cela nous permet de fournir la plupart des services de site Web.",
+            "responsive_Web_text": "Vos visiteurs vérifient-ils votre site Web à l'aide de leurs téléphones intelligents? Tablette? Portable? Peu importe quel appareil, nous rendons le site Web totalement adaptable à tout appareil!",
+            "update_website_contents_text": "Avez-vous seulement besoin de mettre à jour et d'apporter des modifications à votre site Web? Nous pouvons vous offrir un prix très raisonnable pour un service parfait.",
+            "debug_improvement_text": "Y a-t-il un problème sur votre site web? Avez-vous besoin de faire quelque chose de mieux? Parlez-nous, laissez-nous trouver la meilleure solution à un coût raisonnable."
         },
-        "sec3":{
-            "some_of_our_works":"QUELQUES-UNS DE NOS TRAVAUX",
-            "portfolio":"portefeuille",
-            "some_of_our_works_text":"Vous pouvez trouver ici certaines de nos œuvres, y compris la programmation Web, la conception Web, la conception graphique ou la photographie.",
-            "see_all_portfolio":"Voir le portfolio complet",
-            "graphic_design":"graphic design",
-            "web_design_ui":"Web Design (UI)",
-            "email_marketing":"email marketing"
+        "sec3": {
+            "some_of_our_works": "QUELQUES-UNS DE NOS TRAVAUX",
+            "portfolio": "portefeuille",
+            "some_of_our_works_text": "Vous pouvez trouver ici certaines de nos œuvres, y compris la programmation Web, la conception Web, la conception graphique ou la photographie.",
+            "see_all_portfolio": "Voir le portfolio complet",
+            "graphic_design": "graphic design",
+            "web_design_ui": "Web Design (UI)",
+            "email_marketing": "email marketing"
         },
-        "sec4":{
-            "about_us":"à propos de nous",
-            "web_design":"Design Web",
-            "web_development":"Développement Web",
-            "frontend_development":"Développement Frontend",
-            "photography":"La photographie",
-            "gr_design":"Conception graphique",
-            "we_are_expert_in_web_text":"Après des années de formation en informatique, en programmation d'applications, en technologie Web et en infographie, et plus de 10 ans d'expérience dans des projets de grande, moyenne ou petite taille, nous pouvons maintenant affirmer que nous sommes des experts dans certains domaines du web et de l'informatique."
+        "sec4": {
+            "about_us": "à propos de nous",
+            "web_design": "Design Web",
+            "web_development": "Développement Web",
+            "frontend_development": "Développement Frontend",
+            "photography": "La photographie",
+            "gr_design": "Conception graphique",
+            "we_are_expert_in_web_text": "Après des années de formation en informatique, en programmation d'applications, en technologie Web et en infographie, et plus de 10 ans d'expérience dans des projets de grande, moyenne ou petite taille, nous pouvons maintenant affirmer que nous sommes des experts dans certains domaines du web et de l'informatique."
         },
         "sec5": {
-            "meet_the_team":"MEET THE TEAM",
-            "we_are_expert_in_what_we_do":"We Are Expert In What we do!",
-            "programmer_developer":"Programmer / Developer",
-            "farzad_text":"A educated and experienced, web developer/designer. He educated in \"Computer Application Programming\", \"Web Technology\" and \"Infographie\" and also has more than 6 years experiences in the field of Web, Programming and Design.",
-            "sales_manager_accountant":"Sales manager & Accountant",
-            "lili_text":"A perfect sales manager and Financial manager. She educated in \"Theoretical Economy\" and \"Accounting\". She has more than 5 years of experiences as Sales Manager and 2 years as Accounting Technician.",
-            "motivators":"Motivators",
-            "motivators_text":"Céline, is 4 years old and Caroline, is less than a year. They are expert in their role as Life Motivator."
+            "meet_the_team": "MEET THE TEAM",
+            "we_are_expert_in_what_we_do": "We Are Expert In What we do!",
+            "programmer_developer": "Programmer / Developer",
+            "farzad_text": "A educated and experienced, web developer/designer. He educated in \"Computer Application Programming\", \"Web Technology\" and \"Infographie\" and also has more than 6 years experiences in the field of Web, Programming and Design.",
+            "sales_manager_accountant": "Sales manager & Accountant",
+            "lili_text": "A perfect sales manager and Financial manager. She educated in \"Theoretical Economy\" and \"Accounting\". She has more than 5 years of experiences as Sales Manager and 2 years as Accounting Technician.",
+            "motivators": "Motivators",
+            "motivators_text": "Céline, is 4 years old and Caroline, is less than a year. They are expert in their role as Life Motivator."
+        },
+        "sec6": {
+            "social_network": "Réseaux sociaux",
+            "insta_posts": "Articles sur Instagram"
+        },
+        "sec7": {
+            "contact_us": "Contactez nous",
+            "name": "Nom complet",
+            "phone": "Téléphone",
+            "send_message": "Envoyer le message",
+            "err_msg": "Cette case ne peut pas être vide"
         }
     }
 };
 /*
 $texts['get_in_touch'] = ['GET IN TOUCH', 'ENTRER EN CONTACT'];
-$texts['contact_us'] = ['Contact Us', 'Contactez nous'];
-$texts['name'] = ['Name', 'Nom complet'];
-$texts['phone'] = ['Phone', 'Téléphone'];
-$texts['send_message'] = ['Send Message', 'Envoyer le message'];
 $texts['preview_the_project'] = ['Preview the project', 'Prévisualiser le projet'];
-$texts['insta_posts'] = ['Posts in Instagram', 'Articles sur Instagram'];
-$texts['social_network'] = ['Social networks', 'Réseaux sociaux'];
 $texts['privacy_policy'] = ['Privacy Policy', 'Politique de Confidentialité'];
 $texts['back_to_homepage'] = ['Back to Home page', 'Retour à la page d\'accueil'];
     */
